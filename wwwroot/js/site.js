2673826ba60a19273bae6cd5c090221dc1bce1e8
James Long <jameslong595@gmail.com>
2020-11-08 12:52:02 -0500

    Fixed countless HTML/CSS issues arising from conversion from static web pages to MVC framework. Replaced contact us form on About Us page with link in footer. Created a controller for each page.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,14 +1,1 @@
-/**
- * Your JS code here
- */
-
-/**
- * Sample code
- */
-document.addEventListener('DOMContentLoaded', function() {
-  var ulElement = document.getElementById('links');
-  var liElement = document.createElement('li');
-  liElement.appendChild(document.createTextNode('created by main.js'));
-
-  ulElement.appendChild(liElement);
-});
+// Write your JavaScript code

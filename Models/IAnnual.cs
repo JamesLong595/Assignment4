862dfd3100a280ae585b9c8418923ba686f08d76
James Long <jameslong595@gmail.com>
2020-11-28 17:10:50 -0500

    delete unused NewConsumptionRecord.cs; commented out all other unused .cs files

--- Models/IAnnual.cs
+++ Models/IAnnual.cs
@@ -1,19 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
 
-namespace Assignment4.Models
-{
-    public interface IAnnual
-    {
-        IEnumerable<AnnualEnergyConsumption> GetAll();
-        AnnualEnergyConsumption GetbyId(int id);
-        void Add(AnnualEnergyConsumption newconsump);
-        string getsector(int id);
-        int getyear(int id);
-        string getenergysource(int id);
+//namespace Assignment4.Models
+//{
+//    public interface IAnnual
+//    {
+//        IEnumerable<AnnualEnergyConsumption> GetAll();
+//        AnnualEnergyConsumption GetbyId(int id);
+//        void Add(AnnualEnergyConsumption newconsump);
+//        string getsector(int id);
+//        int getyear(int id);
+//        string getenergysource(int id);
 
-        string getvalue(int id);
-    }
-}
+//        string getvalue(int id);
+//    }
+//}
